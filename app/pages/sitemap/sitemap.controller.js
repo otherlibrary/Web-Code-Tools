d81e7adae2e6d93d438d2fbf8ff2e7352db207d2
Jason Macfarlane <jasonmac5@gmail.com>
2016-09-15 14:40:25 -0400

    Update sitemap

--- app/pages/sitemap/sitemap.controller.js
+++ app/pages/sitemap/sitemap.controller.js
@@ -7,24 +7,34 @@
     'htmlGeneratorService',
     'microdataGeneratorService',
     'jsonLdGeneratorService',
+    'openGraphGeneratorService',
 
-    function(cssGeneratorService, htmlGeneratorService, microdataGeneratorService, jsonLdGeneratorService) {
-      var ctrl = this;
-      
-      cssGeneratorService.getCssGenerators().then(function(data) {
-        ctrl.cssGenerators = data;
-      });
+    function(
+      cssGeneratorService,
+      htmlGeneratorService,
+      microdataGeneratorService,
+      jsonLdGeneratorService,
+      openGraphGeneratorService) {
+        var ctrl = this;
+        
+        cssGeneratorService.getCssGenerators().then(function(data) {
+          ctrl.cssGenerators = data;
+        });
 
-      htmlGeneratorService.getHtmlGenerators().then(function(data) {
-        ctrl.htmlGenerators = data;
-      });
+        htmlGeneratorService.getHtmlGenerators().then(function(data) {
+          ctrl.htmlGenerators = data;
+        });
 
-      microdataGeneratorService.getMicrodataGenerators().then(function(data) {
-        ctrl.microdataGenerators = data;
-      });
+        microdataGeneratorService.getMicrodataGenerators().then(function(data) {
+          ctrl.microdataGenerators = data;
+        });
 
-      jsonLdGeneratorService.getJsonLdGenerators().then(function(data) {
-        ctrl.jsonLdGenerators = data;
-      });
+        jsonLdGeneratorService.getJsonLdGenerators().then(function(data) {
+          ctrl.jsonLdGenerators = data;
+        });
+
+        openGraphGeneratorService.getOpenGraphGenerators().then(function(data) {
+          ctrl.openGraphGenerators = data;
+        });
   }]);
 })();
